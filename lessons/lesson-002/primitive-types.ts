e6dff48affeb50376f81d181a2823259a16296d5
jbaldwin <joshrbaldwin@gmail.com>
2024-02-12 20:08:49 -0700

    Add lesson-002 completed typescript file

---
+++ lessons/lesson-002/primitive-types.ts
@@ -0,0 +1,52 @@
+let b1: boolean = true;
+let b2: boolean = false;
+
+console.log(b1);
+console.log(b2);
+console.log(true);
+console.log(false);
+
+let n1: number = 42;
+let n2: number = 3.14;
+let n3 = 1337;
+
+console.log(n1);
+console.log(n2);
+console.log(n3);
+console.log(1234);
+console.log(0.9876);
+
+let s1 = "Hello World";
+let s2 = "";
+let s3 = "1234";
+
+console.log(s1);
+console.log(s2);
+console.log(s3);
+
+let h = "Hello";
+let w = "World";
+let hi = `${h} I'm interpolated`;
+let wi = `${w} I am also!`;
+let hw = `${hi} ${wi}!`;
+
+console.log(h);
+console.log(w);
+console.log(hi);
+console.log(wi);
+console.log(hw);
+
+let a1: number[] = [1, 2, 3, 4, 5];
+let a2: number[] = [];
+let a3 = [true, false, true];
+let a4 = ["Hello", "World"];
+
+let hello = "Hello";
+let world = "World";
+let a5 = [hello, world, "from variables"];
+
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+console.log(a5);
